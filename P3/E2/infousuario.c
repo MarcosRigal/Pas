b7fc40a56360882f9070627915cfdd62f3955839
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-05-03 21:27:45 +0200

    Done E2

--- P3/E2/infousuario.c
+++ P3/E2/infousuario.c
@@ -2,6 +2,7 @@
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -20,6 +21,7 @@ Ejemplos de ejecución para este programa:
 int main(int argc, char *argv[]) {
     // Declaracion de las estructuras que almacenarán la informarción de un usuario y de un grupo
     char *lgn;
+    char *name;
     struct passwd *pw;
     struct group *gr;
 
@@ -31,7 +33,7 @@ int main(int argc, char *argv[]) {
     // de la variable de entorno USER (getenv("USER")) y pasarselo a getpwnam(). Modifique el programa para ello.
     if (argc < 2) {
         //DEVUELVE LA ESTRUCTURA TRAS RECIBIR lgn COMO PARÁMETRO
-        if ((lgn = getlogin()) == NULL || (pw = getpwnam(lgn)) == NULL) {
+        if ((lgn = getenv("USER")) == NULL || (pw = getpwnam(lgn)) == NULL) {
             fprintf(stderr, "Fallo al obtener información de usuario.\n");
             exit(1);
         }
@@ -47,7 +49,7 @@ int main(int argc, char *argv[]) {
     }
 
     //Aqui ya se dispone de la estructura que contiene informacion del usuario
-    printf("Nombre: %s\n", pw->pw_gecos); //No es lo mismo el nombre de usuario asociado a un login que el propio login
+    printf("Nombre: %s\n", strtok(pw->pw_gecos, ",")); //No es lo mismo el nombre de usuario asociado a un login que el propio login
     printf("Login: %s\n", pw->pw_name);
     printf("Password: %s\n", pw->pw_passwd);
     printf("UID: %d\n", pw->pw_uid);
